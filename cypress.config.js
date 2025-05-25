569455626ee7cba6f73a7d2be4b78addc50c505d
Alex-v-p <r0989526@student.thomasmore.be>
2025-05-25 23:51:33 +0200

    cypress cloud with github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'i3mz7u',
+  projectId: '3dr9i1',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
